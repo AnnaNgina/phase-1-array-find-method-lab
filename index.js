c0480993c70f9ebddb28908ae3f4c5909853663e
annastacia <annastaciangina96@gmail.com>
2022-06-13 22:43:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    const dub = record.find(function(winningYearForDenver) {
+        return winningYearForDenver.result === 'W';
+   })
+   return !!dub ? dub.year : dub;
+}
